bb583abd06920da3adabf13e1486e2376f9d37a1
keiff3r <keiffer@blooo.io>
2024-08-09 20:02:59 +0200

    fix: handle stack too small issue on nanoX

--- app/src/common/tx.c
+++ app/src/common/tx.c
@@ -21,14 +21,16 @@
 #include <string.h>
 #include "zxmacros.h"
 
-#if defined(TARGET_NANOX) || defined(TARGET_NANOS2) || defined(TARGET_STAX) || defined(TARGET_FLEX)
+#if defined(TARGET_NANOS2) || defined(TARGET_STAX) || defined(TARGET_FLEX)
 #define RAM_BUFFER_SIZE 8192
+#define FLASH_BUFFER_SIZE 16384
+#elif defined(TARGET_NANOX)
+#define RAM_BUFFER_SIZE 8158
 #define FLASH_BUFFER_SIZE 16384
 #elif defined(TARGET_NANOS)
 #define RAM_BUFFER_SIZE 256
 #define FLASH_BUFFER_SIZE 8192
 #endif
-
 // Ram
 uint8_t ram_buffer[RAM_BUFFER_SIZE];
 
